6a3056dfeb6df4b64b00a02b5e58f7d53a6e59c3
bellafarias <pereira.isabellafarias@gmail.com>
2024-09-06 13:59:50 -0300

    mudanças nos estilos do projeto e integração da pasta assets

---
+++ src/app/components/contact/index.tsx
@@ -0,0 +1,13 @@
+import { Text, TouchableOpacity } from "react-native";
+import { styles } from "./styles"
+import { Avatar } from "../avatar";
+
+export function Contact(){
+    return <TouchableOpacity style={styles.container}>
+        <Avatar name="Pedroso" image={require("@/assets/avatar.jpeg")}/>
+        <Text style={styles.name}>Pedro</Text>
+    </TouchableOpacity>
+}
+   
+
+    
---
+++ src/app/components/contact/styles.ts
@@ -0,0 +1,12 @@
+import { theme } from "@/theme"
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        borderRadius: 7,
+        gap: 7,
+    },
+})
